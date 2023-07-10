4a7f6fc0279e10e067be1fd8db1416a6fe7cff53
carlos8788 <carlos8788@gmail.com>
2023-07-10 10:56:22 -0300

    django_lean

--- apps/home/views.py
+++ apps/home/views.py
@@ -11,18 +11,20 @@ def about(request):
     return render(request, 'pages/about.html')
 
 def formulario(request):
-    contacto_front = request.body
-    contacto_str = contacto_front.decode('utf-8') 
-    contacto_diccionario = json.loads(contacto_str)
-    #e print(type(contacto_diccionario))
-    #print(contacto_diccionario['name'])
-    
-    contacto = Contacto(
-        name = contacto_diccionario['name'],
-        email = contacto_diccionario['email'],
-        subject = contacto_diccionario['subject'],
-        message = contacto_diccionario['message']
-    )
-    contacto.save()
-    return JsonResponse({'success':'OK'}, status=200)
-    
+    try:
+        contacto_front = request.body
+        contacto_str = contacto_front.decode('utf-8') 
+        contacto_diccionario = json.loads(contacto_str)
+        #e print(type(contacto_diccionario))
+        #print(contacto_diccionario['name'])
+        
+        contacto = Contacto(
+            name = contacto_diccionario['name'],
+            email = contacto_diccionario['email'],
+            subject = contacto_diccionario['subject'],
+            message = contacto_diccionario['message']
+        )
+        contacto.save()
+        return JsonResponse({'success':'OK'}, status=200)
+    except Exception as e:
+        print(e)
